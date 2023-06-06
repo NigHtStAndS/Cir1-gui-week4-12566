31c8475eb758698a4e44d8d923e9bc7a45bb6e0f
phisit77 <kachidoki787@gmail.com>
2023-06-06 13:35:04 +0700

    firstinintiel

---
+++ bin/dart_list.dart
@@ -0,0 +1,9 @@
+
+void main () {
+  List<String> vnl2023 = ['Thailand','Japan','korean'];
+
+for (int index = 0; index < vnl2023.length; index++) {
+   print(vnl2023[index]);
+}
+
+}
---
+++ bin/dart_week4_dart_week4.dart
@@ -0,0 +1,8 @@
+void main () {
+List<int> number = [10,20,30,40];
+  print(number);
+  print(number[0]);
+  print(number[1]);
+  print(number[2]);
+  print(number[3]);
+}
